d764c33d99c960086e5783c8923874ba4ff25e1f
revanth-collab <gurramnagarevanth1@gmail.com>
2024-10-04 09:50:28 +0530

    Update SearchBar.js

--- book-library-frontend/src/components/SearchBar.js
+++ book-library-frontend/src/components/SearchBar.js
@@ -1,4 +1,7 @@
 import React, { useState } from 'react';
+import {Link} from "react-router-dom"
+
+import "./books.css"
 
 const SearchBar = ({ onSearch }) => {
   const [query, setQuery] = useState('');
@@ -9,16 +12,23 @@ const SearchBar = ({ onSearch }) => {
   };
 
   return (
-    <form onSubmit={handleSearch}>
+    <>
+    <form onSubmit={handleSearch} className='search-container'>
       <input 
         type="text" 
         placeholder="Search by title, author, or genre" 
         value={query} 
         onChange={(e) => setQuery(e.target.value)} 
+        className='search'
       />
-      <button type="submit">Search</button>
+      <button type="submit" className='button'>Search</button>
+      <Link to={`/my-library`}>
+      <button className='button'>My Library</button>
+    </Link>
     </form>
+    </>
   );
 };
 
 export default SearchBar;
+
