d651fb4ae983808c0a0b9fc796f05aaf12a2d26e
revanth-collab <gurramnagarevanth1@gmail.com>
2024-10-04 09:48:50 +0530

    Update MyLibraryPage.js

--- book-library-frontend/src/pages/MyLibraryPage.js
+++ book-library-frontend/src/pages/MyLibraryPage.js
@@ -1,6 +1,9 @@
 // src/pages/MyLibraryPage.js
 import React, { useContext } from 'react';
 import { LibraryContext } from '../context/LibraryContext';
+import {Link} from "react-router-dom"
+
+import "./pages.css"
 
 const MyLibraryPage = () => {
   const { myLibrary, removeFromLibrary } = useContext(LibraryContext);
@@ -10,22 +13,29 @@ const MyLibraryPage = () => {
   };
 
   if (myLibrary.length === 0) {
-    return <div>Your library is empty. Add some books!</div>;
+    return <div className='error'><h1 className='error-message'>Your library is empty. Add some books!</h1>
+    <Link to={`/`}>
+    <button className='button'>back</button>
+    </Link></div>;
   }
 
   return (
-    <div>
+    <div className='library'>
       <h1>My Library</h1>
-      <ul>
+      <ul className='unlist'>
         {myLibrary.map((book) => (
-          <li key={book.id}>
-            {book.title} by {book.author}
-            <button onClick={() => handleRemoveFromLibrary(book.id)}>Remove</button>
+          <li key={book.id} className='library-list'>
+            <h1 className='library-title'><span className='title'>{book.title}</span> by {book.author}</h1>
+            <button onClick={() => handleRemoveFromLibrary(book.id)} className='button'>Remove</button>
           </li>
         ))}
       </ul>
+      <Link to={`/`}>
+      <button className='button'>Back</button>
+      </Link>
     </div>
   );
 };
 
 export default MyLibraryPage;
+
