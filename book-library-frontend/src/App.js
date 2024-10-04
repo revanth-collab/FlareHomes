37fcb5b2c753fd235eb9e3f5f8c6481f1c90739e
revanth-collab <gurramnagarevanth1@gmail.com>
2024-10-04 09:47:22 +0530

    Update App.js

--- book-library-frontend/src/App.js
+++ book-library-frontend/src/App.js
@@ -1,26 +1,3 @@
-// import React from 'react';
-// import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-// import { LibraryProvider } from './context/LibraryContext';
-// import HomePage from './pages/HomePage';
-// import BookDetailsPage from './pages/BookDetailsPage';
-// import MyLibraryPage from './pages/MyLibraryPage';
-
-// const App = () => {
-//   return (
-//     <LibraryProvider>
-//       <Router>
-//         <Switch>
-//           <Route path="/" exact component={HomePage} />
-//           <Route path="/books/:id" component={BookDetailsPage} />
-//           <Route path="/my-library" component={MyLibraryPage} />
-//         </Switch>
-//       </Router>
-//     </LibraryProvider>
-//   );
-// };
-
-// export default App;
-
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { LibraryProvider } from './context/LibraryContext';
@@ -43,3 +20,4 @@ const App = () => {
 };
 
 export default App;
+
