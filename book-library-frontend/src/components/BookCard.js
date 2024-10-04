94a9dee328fe7129db35c36ab999cc721bab76e5
revanth-collab <gurramnagarevanth1@gmail.com>
2024-10-04 09:51:38 +0530

    Update BookCard.js

--- book-library-frontend/src/components/BookCard.js
+++ book-library-frontend/src/components/BookCard.js
@@ -1,16 +1,20 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+import "./books.css"
+
 const BookCard = ({ book }) => {
+  const {title,author,genre,rating} =book
   return (
     <div className="book-card">
-      <h3>{book.title}</h3>
-      <p>Author: {book.author}</p>
-      <p>Genre: {book.genre}</p>
-      <p>Rating: {book.rating}</p>
+      <h3>{title}</h3>
+      <p>Author: {author}</p>
+      <p>Genre: {genre}</p>
+      <p>Rating: {rating}</p>
       <Link to={`/books/${book.id}`}>View Details</Link>
     </div>
   );
 };
 
 export default BookCard;
+
